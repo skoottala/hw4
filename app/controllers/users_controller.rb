4486dc3dd722e21ea6f1522c3d9c96938b0e8a6c
skoottala <sruthy.koottala@kellogg.northwestern.edu>
2022-03-14 16:39:17 +0000

    bcrypt and views session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
+  #params["user"]: "user"=>{"username"=>"Skoott2", "email"=>"skoott2@email.com", "password"=>"[FILTERED]"}
   def create
+    plain_text_password= params["user"]["password"]
     @user = User.new(params["user"])
+    @user.password= BCrypt::Password.create(plain_text_password)
     @user.save
     redirect_to "/"
   end
