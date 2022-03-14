7aae2fe9b677ebb1220bbfe4297f9f8f71a9b402
skoottala <sruthy.koottala@kellogg.northwestern.edu>
2022-03-14 17:35:18 +0000

    flash notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email= params["email"]
+    entered_password=params["password"]
+
+    #checking email
+    @user=User.find_by({email:entered_email})
+
+    #checking password
+    if @user
+      if BCrypt::Password.new(@user.password)== entered_password
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
+      else
+        flash[:notice] = "Password is incorrect."
+        redirect_to "/sessions/new"
+      
+      end
+    else
+      flash[:notice] = "Username not found"
+      redirect_to "/sessions/new" #back to login page
+    end
+
   end
 
   def destroy
